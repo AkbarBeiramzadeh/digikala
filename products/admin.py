7202aef45d312a9e068d78e48e21d6f7ee654695
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-06-07 12:58:21 +0330

    added ProductAdmin ProductImageInline CategoryAdmin ImageAdmin classes to admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Product, Category, Comment, Question, Answer,\
+    Image, ProductOption, ProductPrice
 
 # Register your models here.
+
+
+class ProductImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'en_name', 'name', 'category']
+    list_filter = ['category']
+    search_fields = ['en_name', 'name']
+    inlines = (ProductImageInline,)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug', 'parent']
+    list_filter = ['parent']
+    search_fields = ['name', 'description']
+    fieldsets = (
+        ('details', {
+            'fields': ('name', 'slug', 'parent',
+                       ('description',),)
+        }),
+        ('images', {
+            'fields': ('icon', 'image',)
+        }),
+
+    )
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
