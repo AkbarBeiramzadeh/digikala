f31b0b0e4a9b81b9c998a222da6498605df97a76
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-06-07 11:10:15 +0330

    create Digikala project komijani

---
+++ digikala_komijani/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for digikala_komijani project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digikala_komijani.settings')
+
+application = get_wsgi_application()
